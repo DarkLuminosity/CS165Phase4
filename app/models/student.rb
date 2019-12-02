d0a19ad432c65c149c91311abfcec63ac479bdf1
dark_luminosity <engelbergsee@yahoo.com>
2019-12-03 00:57:41 +0800

    sixth

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :highschool
   belongs_to :testcenter
+  SEX = ["Male", "Female"]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_082033) do
+ActiveRecord::Schema.define(version: 2019_12_02_150044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_082033) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "sex"
-    t.date "dateofbirth"
+    t.datetime "dateofbirth"
     t.bigint "cellphonenumber"
     t.string "address"
     t.integer "highschool_id", null: false
